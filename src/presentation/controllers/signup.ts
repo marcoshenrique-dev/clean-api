3f39548d4668185512aa6b14b560622e3fb54048
Marcos Henrique <marcoshenriqueh393@gmail.com>
2020-11-09 15:57:41 -0300

    chore: config standard in eslint

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,22 +1,20 @@
-import { HttpRequest, HttpResponse } from '../protocols/http';
-import { MissingParamError } from '../errors/missing-param-error';
-import { badRequest } from '../helpers/http-helper';
-import { Controller } from '../protocols/controller';
+import { HttpRequest, HttpResponse } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
+import { Controller } from '../protocols/controller'
 
 export class SignupController implements Controller {
-   handle(httpRequest: HttpRequest): HttpResponse {
-
-      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-      for(const field of requiredFields){
-         if (!httpRequest.body[field]){
-            return badRequest(new MissingParamError(field))  
-         }
+  handle (httpRequest: HttpRequest): HttpResponse {
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
       }
+    }
 
-      return{
-         statusCode: 200,
-         body: 'success'
-      } 
-      
-   }
+    return {
+      statusCode: 200,
+      body: 'success'
+    }
+  }
 }
